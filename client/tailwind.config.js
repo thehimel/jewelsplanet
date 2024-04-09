809760abedc76b13a738cb3f5254e5d6c8dd0732
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-09 23:32:27 +0200

    configure tailwind.config.js for next ui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import {nextui} from "@nextui-org/react";
+
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
