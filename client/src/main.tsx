581bff0530bec3304d61a73e0f31e3c414a56103
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-11 01:42:01 +0200

    configured redux store with redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from "@nextui-org/react";
+import {Provider} from "react-redux";
 import App from './App.tsx'
 import './index.css'
+import store, {persistor} from "./store/store.ts";
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <NextUIProvider>
-    <App />
-  </NextUIProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </PersistGate>
+  </Provider>
 )
