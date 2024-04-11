17620327968316747c7fd046b7ba36a88afdcb4e
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-11 03:27:41 +0200

    configured nextui theme with ThemeSwitcher

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
+import {useTheme} from "next-themes";
+import {useEffect} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Home.tsx";
+import {useAppSelector} from "./store/hooks.ts";
 import NavigationBar from "./units/NavigationBar.tsx";
 
 const App = () => {
+  const darkMode = useAppSelector((state) => state.base.darkMode);
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    setTheme(darkMode ? 'dark' : 'light');
+  }, [darkMode, setTheme]);
+
   return (
     <BrowserRouter>
       <main>
