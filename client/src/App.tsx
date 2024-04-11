be7979398948d4831fcd0d89fded316b366d4bb1
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-11 02:30:50 +0200

    init navigation bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Home.tsx";
+import NavigationBar from "./units/NavigationBar.tsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <main>
+        <NavigationBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
