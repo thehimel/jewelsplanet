44fd8f21b87f3482200e107bc8432b9f32df71b9
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-12 18:08:28 +0200

    updated Footer

--- client/src/components/Navigation/Footer.tsx
+++ client/src/components/Navigation/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-0 w-full border-t py-4">
+    <footer className="w-full border-t py-4">
       <div className="container mx-auto flex justify-center">
         <p>JewelsPlanet &copy; {year}</p>
       </div>
