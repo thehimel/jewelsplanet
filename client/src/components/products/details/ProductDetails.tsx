28544b8a72014df95399e08742e0159b7eaa9901
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-13 16:55:15 +0200

    added image in product details page

--- client/src/components/products/details/ProductDetails.tsx
+++ client/src/components/products/details/ProductDetails.tsx
@@ -1,9 +1,15 @@
+import ProductImage from "@/components/products/details/ProductImage.tsx";
 import Purchase from "@/components/products/details/Purchase.tsx";
 
 const ProductDetails = () => {
   return (
-    <div>
-      <Purchase/>
+    <div className="flex flex-wrap justify-center w-full items-center gap-4 p-4 md:p-16 ">
+      <div className="flex">
+        <ProductImage/>
+      </div>
+      <div className="flex">
+        <Purchase/>
+      </div>
     </div>
   )
 }
---
+++ client/src/components/products/details/ProductImage.tsx
@@ -0,0 +1,34 @@
+import {
+  Card,
+} from "@/components/ui/card.tsx"
+import '../styles/styles.css'
+import {Image} from "@nextui-org/react";
+import {useState, MouseEvent} from "react";
+
+const ProductImage = () => {
+  const [transformOrigin, setTransformOrigin] = useState('50% 50%');
+
+  const handleMouseMove = (event: MouseEvent<HTMLDivElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const offsetY = event.clientY - rect.top;
+
+    const percentX = (offsetX / rect.width) * 100;
+    const percentY = (offsetY / rect.height) * 100;
+
+    setTransformOrigin(`${percentX}% ${percentY}%`);
+  };
+
+  return (
+    <Card className="w-[350px] relative overflow-hidden" onMouseMove={handleMouseMove}>
+      <Image
+        alt="Product Image"
+        className="transition-transform duration-300 transform hover:scale-zoom hover:cursor-plus rounded-none"
+        src="https://images.pexels.com/photos/1454172/pexels-photo-1454172.jpeg"
+        style={{ transformOrigin }}
+      />
+    </Card>
+  )
+}
+
+export default ProductImage;
