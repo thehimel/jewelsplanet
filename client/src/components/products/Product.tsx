5b08045caee851f0787fe5fe21639fb96b8db093
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-13 19:04:30 +0200

    updated Product.tsx

--- client/src/components/products/Product.tsx
+++ client/src/components/products/Product.tsx
@@ -3,17 +3,17 @@ import {Card, CardHeader, Image, CardBody} from "@nextui-org/react";
 
 const Product = () => {
   return (
-    <Card className="pt-1.5">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+    <Card>
+      <CardHeader className="p-2 px-4 flex-col items-start">
         <h4 className="font-bold text-large">Frontend Radio</h4>
         <p className="text-tiny uppercase font-bold text-default-500">Daily Mix</p>
         <small className="">$ 900</small>
         <Rating value={4.5} count={16}/>
       </CardHeader>
-      <CardBody className="overflow-visible pt-1">
+      <CardBody className="overflow-visible p-0">
         <Image
           alt="Card background"
-          className="object-cover rounded-xl"
+          className="object-cover rounded-none"
           src="https://images.pexels.com/photos/1454172/pexels-photo-1454172.jpeg"
           width={270}
         />
