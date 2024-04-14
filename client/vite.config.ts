3f0f8667a9c7c411c3add617ee701ebeee70f4d9
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-04-14 11:32:59 +0200

    client: configure proxy for server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,8 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_URL = process.env.API_URL || 'http://127.0.0.1:8000';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,5 +14,13 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: API_URL,
+        changeOrigin: true,
+      }
+    }
   },
 })
